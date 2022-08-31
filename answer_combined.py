f926d7bce1fe7092e33a64295031fd0707a53e7e
Gordon <g.j.d.arscott@tilburguniversity.edu>
2022-08-31 13:03:23 +0200

    This is the upload for the completed test file

---
+++ answer_combined.py
@@ -0,0 +1,31 @@
+
+###### Question 1:
+
+def fix_count_pos_neg(input_list):
+    # paste your code here
+
+##### Question 2:
+
+def gibberish(word):
+    # paste your code here
+
+##### Question 3:
+
+def make_smoothie(ingredients):
+    # paste your code here
+
+##### Question 4:
+
+def furniture_cost(furniture_prices):
+    # paste your code here
+
+##### Question 5:
+
+def extract_header(filedir,filename):
+    # paste your code here
+
+##### Question 6:
+
+def sort_by_followers(twitter_info):
+    # paste your code here
+
